17c481881ca69d0d29091831558fdd647e0f03a9
Paw565pl <paw565pl@gmail.com>
2023-07-27 13:10:34 +0200

    changed validation messages

--- frontend/src/schemas/MovieSchema.ts
+++ frontend/src/schemas/MovieSchema.ts
@@ -4,11 +4,11 @@ const MovieSchema = z.object({
   title: z.string().nonempty("this field is required"),
   genre: z.string().nonempty("this field is required"),
   numberInStock: z
-    .number({ invalid_type_error: "this must be a number" })
+    .number({ invalid_type_error: "this field is required" })
     .min(0, "must be greater than 0")
     .max(100, "must be less than 100"),
   rate: z
-    .number({ invalid_type_error: "this must be a number" })
+    .number({ invalid_type_error: "this field is required" })
     .min(0, "must be greater than 0")
     .max(10, "must be greater than 10"),
 });
