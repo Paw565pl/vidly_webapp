7f78b1a6e50abbe4ae88ea6b8e2053d903596b05
Paw565pl <paw565pl@gmail.com>
2023-07-28 15:35:06 +0200

    added react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes.tsx";
 import "./index.css";
+import router from "./routes.tsx";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 60 } },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools></ReactQueryDevtools>
+    </QueryClientProvider>
   </React.StrictMode>
 );
