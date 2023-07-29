51ece57160a0c19b14f0ca0ec3cd4b21eda01d5b
Paw565pl <paw565pl@gmail.com>
2023-07-29 13:20:01 +0200

    auto redirect

--- frontend/src/hooks/useAddMovie.ts
+++ frontend/src/hooks/useAddMovie.ts
@@ -14,6 +14,8 @@ const useAddMovie = () => {
       queryClient.invalidateQueries({
         queryKey,
       }),
+
+    retry: false,
   });
 };
 
--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => navigate("/"), 3000);
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Not Found <Spinner animation="border" variant="primary" />
+      </h1>
+      <p>Redirecting you back to the main page...</p>
+    </>
+  );
 };
 
 export default NotFound;
