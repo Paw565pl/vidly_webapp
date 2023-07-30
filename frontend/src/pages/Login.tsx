a42c889c965bdec7600eb600f797343cf6935a57
Paw565pl <paw565pl@gmail.com>
2023-07-30 17:03:51 +0200

    added error toast

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,38 +1,47 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { FieldValues, useForm } from "react-hook-form";
 import Input from "../components/common/Input";
+import ToastComponent from "../components/common/ToastComponent";
 import { UserLoginData, loginSchema } from "../entities/Auth";
 
 const LoginForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<UserLoginData>({ resolver: zodResolver(loginSchema) });
+  const [errorVisibility, setErrorVisibility] = useState(false);
+
+  const { register, handleSubmit } = useForm<UserLoginData>({
+    resolver: zodResolver(loginSchema),
+  });
 
   const submitAction = (data: FieldValues) => {
     console.log(data);
   };
 
+  const submitFail = () => {
+    setErrorVisibility(true);
+    setTimeout(() => {
+      setErrorVisibility(false);
+    }, 5100);
+  };
+
   return (
     <div>
+      {errorVisibility && (
+        <ToastComponent bg="danger">
+          Either email or password are wrong.
+        </ToastComponent>
+      )}
+
       <h1>Login</h1>
-      <Form onSubmit={handleSubmit(submitAction)}>
-        <Input
-          id="loginEmail"
-          register={register("email")}
-          autofocus={true}
-          errorMessage={errors?.email?.message}
-        >
+      <Form onSubmit={handleSubmit(submitAction, submitFail)}>
+        <Input id="loginEmail" register={register("email")} autofocus={true}>
           Email
         </Input>
         <Input
           id="loginPassword"
           register={register("password")}
           type={"password"}
-          errorMessage={errors?.password?.message}
         >
           Password
         </Input>
