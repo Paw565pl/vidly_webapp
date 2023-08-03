a35ce2da4d133d5baff0e8d754aa4a528e3e330c
Paw565pl <paw565pl@gmail.com>
2023-08-03 19:37:57 +0200

    dockerized the app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    hmr: {
+      clientPort: 5173,
+    },
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
