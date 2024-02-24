0f547d5ad90547900dc8a31187f5626e085e6d9d
Paw565pl <paw565pl@gmail.com>
2024-02-24 15:51:37 +0100

    expressed timeout with ms utility

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ms from "ms";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -8,14 +9,14 @@ import "./index.css";
 import router from "./routes.tsx";
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 1000 * 60 * 60 } },
+  defaultOptions: { queries: { staleTime: ms("1h") } },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools></ReactQueryDevtools>
+      <ReactQueryDevtools />
     </QueryClientProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
